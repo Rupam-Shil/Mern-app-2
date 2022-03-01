cf1bf686cdca013dc286a629f34d9c0fd83d764e
Rupam Shil <rupamshil111@gmail.com>
2022-03-01 21:25:51 +0530

    xx

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,21 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 // @desc 	Register new user
 // @route 	POST /api/users
 // @access 	Public
 exports.registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error('Please add all fields');
+	}
+	const userExist = await User.find({ email });
+	if (userExist) {
+		res.status(400);
+		throw new Error('User already exists');
+	}
 	res.json({ message: 'register users' });
 });
 // @desc 	Login user
