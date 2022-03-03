fdb39abe84f782de88ad1fde22cdcfb767fde20a
Rupam Shil <rupamshil111@gmail.com>
2022-03-03 11:31:16 +0530

    xx

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 // @desc 	Get goals
 // @route 	GET /api/goals
 // @access 	Private
 exports.getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find({});
+	const goals = await Goal.find({ user: req.user.id });
 	res.status(200).json(goals);
 });
 // @desc 	Set goals
@@ -17,6 +18,7 @@ exports.setGoal = asyncHandler(async (req, res) => {
 	}
 	const goal = await Goal.create({
 		text: req.body.text,
+		user: req.user.id,
 	});
 	res.status(200).json(goal);
 });
@@ -24,11 +26,19 @@ exports.setGoal = asyncHandler(async (req, res) => {
 // @route 	PUT /api/goals/:id
 // @access 	Private
 exports.updateGoal = asyncHandler(async (req, res) => {
-	const { id } = req.params;
-	const goal = await Goal.findById(id);
+	const goal = await Goal.findById(req.params.id);
 	if (!goal) {
 		res.status(400);
 		throw new Error('Goal not found');
+	}
+	const user = await User.findById(req.user.id);
+	if (!user) {
+		res.status(401);
+		throw new Error('User not found');
+	}
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error('User not authorized');
 	}
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
@@ -39,11 +49,20 @@ exports.updateGoal = asyncHandler(async (req, res) => {
 // @route 	DELETE /api/goals/:id
 // @access 	Private
 exports.deleteGoal = asyncHandler(async (req, res) => {
-	const { id } = req.params;
-	const goal = await Goal.findById(id);
+	const goal = await Goal.findById(req.params.id);
+
 	if (!goal) {
 		res.status(400);
 		throw new Error('Goal not found');
+	}
+	const user = await User.findById(req.user.id);
+	if (!user) {
+		res.status(401);
+		throw new Error('User not found');
+	}
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error('User not authorized');
 	}
 	await goal.remove();
 	res.status(200).json({
--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+
 //controllers
 const {
 	getGoals,
@@ -8,7 +10,7 @@ const {
 	deleteGoal,
 } = require('../controllers/goalController');
 
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal);
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
